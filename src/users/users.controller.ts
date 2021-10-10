61c8f26f08339165ce800f2f65f732d04aacb5ec
kim-hyunjin <hyunjin1612@gmail.com>
2021-10-10 21:40:18 +0900

    users 컨트롤러 수정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,15 @@
 import {
   Body,
   Controller,
+  ForbiddenException,
   Get,
+  NotFoundException,
   Post,
   Res,
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiCookieAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { LoggedInGuard } from 'src/auth/logged-in.guard';
 import { NotLoggedInGuard } from 'src/auth/not-logged-in.guard';
@@ -20,8 +22,9 @@ import { UsersService } from './users.service';
 @ApiTags('USERS')
 @Controller('api/users')
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
+  @ApiCookieAuth('connect.sid')
   @ApiOperation({ summary: '내 정보 가져오기' })
   @Get()
   async getProfile(@User() user) {
@@ -31,17 +34,36 @@ export class UsersController {
   @ApiOperation({ summary: '로그인' })
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@User() user) {}
+  async login(@User() user) {
+    return user;
+  }
 
   @ApiOperation({ summary: '회원가입' })
   @UseGuards(NotLoggedInGuard)
   @Post()
   async join(@Body() data: JoinRequestDto) {
-    await this.userService.join(data.email, data.nickname, data.password);
+    const user = this.usersService.findByEmail(data.email);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    const result = await this.usersService.join(
+      data.email,
+      data.nickname,
+      data.password,
+    );
+    if (result) {
+      return 'ok';
+    } else {
+      throw new ForbiddenException();
+    }
   }
 
+  @ApiCookieAuth('connect.sid')
   @ApiOperation({ summary: '로그아웃' })
   @UseGuards(LoggedInGuard)
   @Post('logout')
-  async logout(@Res() res) {}
+  async logout(@Res() res) {
+    res.clearCookie('connect.sid', { httpOnly: true });
+    return res.send('ok');
+  }
 }
